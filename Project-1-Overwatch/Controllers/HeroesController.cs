954d4cfe37bced995ffc40324e549fc23ae18c4d
axchris <alexmarcuschristensen@gmail.com>
2017-11-09 16:51:43 -0700

    Added the heroes details view and set up the dynamic nature of it. Added the BreadCrumb for the Heroes page and the heroDetails page.

--- Project-1-Overwatch/Controllers/HeroesController.cs
+++ Project-1-Overwatch/Controllers/HeroesController.cs
@@ -9,9 +9,15 @@ namespace Project_1_Overwatch.Controllers
     public class HeroesController : Controller
     {
         // GET: Heroes
+        public ActionResult heroDetails(string name)
+        {
+            ViewBag.name = name;
+            return View();
+        }
         public ActionResult Index()
         {
             return View();
         }
+        
     }
 }
